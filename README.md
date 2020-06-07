bdc1ce2dd2393a18b2388db57b952ff0297644d0
Thomas Haessle <thaessle@cutii.io>
2020-06-07 23:06:05 +0200

    :memo:

---
+++ README.md
@@ -0,0 +1,1 @@
+# Install script for my macOS
---
+++ install.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew update
+
+brew cask install alfred
+brew cask install balenaetcher
+brew cask install dash
+brew cask install discord
+brew cask install docker
+brew cask install dotnet-sdk
+brew cask install firefox
+brew cask install gimp
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install keka
+brew cask install microsoft-edge
+brew cask install pencil
+brew cask install pgadmin4
+brew cask install postgres
+brew cask install postman
+brew cask install slack
+brew cask install spotify
+brew cask install visual-studio-code
+brew cask install vlc
+brew cask install zoom
+
+brew install asdf
+brew install balena-cli
+brew install coreutils
+brew install git 
+brew install gpatch
+brew install gpg
+brew install jq
+brew install opam
+brew install redis
+brew install rustup-init
+brew install sox
+brew install zola
+
+echo ". $(brew --prefix asdf)/asdf.sh" >> ~/.zshrc
+
+asdf update
+
+asdf plugin-add java https://github.com/halcyon/asdf-java.git
+asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf plugin-add python https://github.com/danhper/asdf-python.git
+
+asdf install java latest
+asdf install nodejs latest
+asdf install ruby 2.4.5
+asdf install python 3.6.2
+asdf install python 2.7.13
+
+asdf global java latest
+. ~/.asdf/plugins/java/set-java-home.zsh
+asdf global nodejs latest
+asdf global ruby 2.4.5
+asdf global python 3.6.2 2.7.13
+
+npm i -g npm
